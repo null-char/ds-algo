bf4a06d537db2a0b63d174d341b29d62b7cd1c4a
SKP <skp.frl@gmail.com>
2020-08-23 21:37:24 +0530

    style: reformat

--- algs/fctrl.cpp
+++ algs/fctrl.cpp
@@ -6,24 +6,32 @@
 // and calculates how many trailing zeroes N! would have.
 
 /// Finds number of trailing zeroes of the factorial of the input integer n.
-int findTrailingZeroes(int n) {
-  int tz = 0;
+int findTrailingZeroes(int n)
+{
+    int tz = 0;
 
-  while (n >= 1) {
-    n /= 5;
-    tz += n;
-  }
+    // The intuition here is to find the number of multiples of 5 that composes
+    // n!. The multiples of 2 will be in far greater amount so we can instead
+    // focus on finding multiples of 5. For some number x, the number of trailing
+    // zeroes is described by the number of times x can be exactly divided by 10.
+    while (n >= 1)
+    {
+        n /= 5;
+        tz += n;
+    }
 
-  return tz;
+    return tz;
 }
 
-int main() {
-  int T;
-  scanf("%u", &T);
+int main()
+{
+    int T;
+    scanf("%u", &T);
 
-  for (auto i = 0; i < T; i++) {
-    int n;
-    scanf("%u", &n);
-    printf("%u\n", findTrailingZeroes(n));
-  }
+    for (auto i = 0; i < T; i++)
+    {
+        int n;
+        scanf("%u", &n);
+        printf("%u\n", findTrailingZeroes(n));
+    }
 }
